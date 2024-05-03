d19f4ba55e31a1a22b4b600f3183301ec8f8b783
Smirkey <guillaume.bertrand59@gmail.com>
2024-05-04 01:02:22 +0200

    chore: use ruff, create simple make rules and add a fix for selenium

--- aiden_app/tools/utils/chrome_driver.py
+++ aiden_app/tools/utils/chrome_driver.py
@@ -11,6 +11,7 @@ class ChromeDriver:
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
+        options.add_argument('--disable-dev-shm-usage')
 
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
